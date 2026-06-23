c14e24078c746e451be4904095ea60d826f2a972
aidan102004 <aidan.abidi@gmail.com>
2026-06-23 18:09:39 +1000

    Initial commit

---
+++ build/CMakeFiles/shell.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shell.dir/src/main.cpp.obj"
+  "CMakeFiles/shell.dir/src/main.cpp.obj.d"
+  "libshell.dll.a"
+  "shell.exe"
+  "shell.exe.manifest"
+  "shell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
